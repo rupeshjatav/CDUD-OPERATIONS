ff2ad9646825696e81fb455ba9b52f6620db3af5
rupeshjatav <rupeshjatav22@gmail.com>
2021-08-02 00:56:50 +0530

    make it better

--- express/express1.js
+++ express/express1.js
@@ -49,8 +49,7 @@ app.use((req, res, next) => {
 
 mongoose.connect('mongodb://localhost:27017/onlineshopping', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        const port = Process.env.PORT || 3000;
-        app.listen(port, () => {
+        app.listen(3000, () => {
             console.log('Server is running on 3000');
         });
     })
